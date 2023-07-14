f6f1abd7d5b7e691e0b445c99ef3c572960b1123
seanperry <seanep94@gmail.com>
2023-07-14 13:53:38 -0700

    Added html and static

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-
+from flask import Flask, render_template
+#export FLASK_APP=app.py ; export FLASK_ENV=development ;flask run
 
 app = Flask(__name__)
 
 
 # Main page
 @app.route("/")
-def hello_world() -> str:
-    return "<p>Hello, World!</p>"
+def hello_world() :
+    return render_template('homepage.html')
